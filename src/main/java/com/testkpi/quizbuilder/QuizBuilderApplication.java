b0f66fde37404817ec7e320f9f06bb4c4a19236e
dimitri <mamchenko2210@gmail.com>
2024-03-29 17:11:19 +0100

    project creation

---
+++ src/main/java/com/testkpi/quizbuilder/QuizBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.testkpi.quizbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testkpi/quizbuilder/QuizBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testkpi.quizbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b559a6fc88e0416c048704451cb91e8312ed205b
dimitri <mamchenko2210@gmail.com>
2024-04-05 21:55:50 +0200

    UserException and UserExceptionHandler added to handle register errors

---
+++ src/main/java/com/testkpi/quizbuilder/exception/UserException.java
@@ -0,0 +1,16 @@
+package com.testkpi.quizbuilder.exception;
+
+public class UserException extends RuntimeException {
+
+    public UserException(String message) {
+        super(message);
+    }
+
+    public UserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/testkpi/quizbuilder/exception/UserExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.testkpi.quizbuilder.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class UserExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleException(UserException exc) {
+
+        UserErrorResponse error = new UserErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleException(Exception exc) {
+
+        UserErrorResponse error = new UserErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}

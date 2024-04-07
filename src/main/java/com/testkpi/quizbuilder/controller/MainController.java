f037e4602282f24308f846307353e08240aba76a
dimitri <mamchenko2210@gmail.com>
2024-04-07 15:20:50 +0200

    username sending to page

--- src/main/java/com/testkpi/quizbuilder/controller/MainController.java
+++ src/main/java/com/testkpi/quizbuilder/controller/MainController.java
@@ -1,6 +1,10 @@
 package com.testkpi.quizbuilder.controller;
 
+import com.testkpi.quizbuilder.response.PageResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @GetMapping("/home")
-    public String getHomePage() {
-        return "some admin things";
+    public ResponseEntity<PageResponse> getHomePage() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        PageResponse pageResponse = new PageResponse(HttpStatus.OK.value(), username, System.currentTimeMillis());
+        return new ResponseEntity<>(pageResponse, HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('ADMIN')")

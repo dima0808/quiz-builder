a54fa250b8d656ef818c63e7aa5635a4a6cfcc35
dimitri <mamchenko2210@gmail.com>
2024-04-07 14:51:23 +0200

    login update

--- src/main/java/com/testkpi/quizbuilder/controller/MainController.java
+++ src/main/java/com/testkpi/quizbuilder/controller/MainController.java
@@ -2,10 +2,17 @@ package com.testkpi.quizbuilder.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class MainController {
+
+    @GetMapping("/home")
+    public String getHomePage() {
+        return "some admin things";
+    }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")

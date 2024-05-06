d0cfe56da3b69d783c0e87855e6533bfdabebabe
dimitri <mamchenko2210@gmail.com>
2024-05-06 02:18:40 +0200

    added basic feedback logic

---
+++ src/main/java/com/testkpi/quizbuilder/controller/FeedbackController.java
@@ -0,0 +1,37 @@
+package com.testkpi.quizbuilder.controller;
+
+import com.testkpi.quizbuilder.entity.Feedback;
+import com.testkpi.quizbuilder.payload.FeedbackDto;
+import com.testkpi.quizbuilder.response.OperationResponse;
+import com.testkpi.quizbuilder.service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/feedback")
+public class FeedbackController {
+
+    private final FeedbackService feedbackService;
+
+    @Autowired
+    public FeedbackController(FeedbackService feedbackService) {
+        this.feedbackService = feedbackService;
+    }
+
+    @GetMapping
+    public List<Feedback> getAllQuestions() {
+        return feedbackService.findAllQuestions();
+    }
+
+    @PostMapping(value = {"/add", "/create"})
+    public ResponseEntity<OperationResponse> addQuestion(@RequestBody FeedbackDto feedbackDto) {
+        feedbackService.saveQuestion(feedbackDto);
+        OperationResponse feedbackResponse = new OperationResponse(HttpStatus.CREATED.value(),
+                "Question has been sent to the administration.", System.currentTimeMillis());
+        return new ResponseEntity<>(feedbackResponse, HttpStatus.CREATED);
+    }
+}
